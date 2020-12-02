d75dfee1b833115f37fd115086c4e57822e3d5ba
Tracy Marshall <tracythedev@gmail.com>
2020-12-02 17:12:17 -0500

    added session storage functionality with redux-persist

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import store from './redux/store'
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
