ae14d3041c01a15cf098dac88846e3a4ee891bcd
Tracy Marshall <tracythedev@gmail.com>
2020-12-05 00:50:00 -0500

    updating stripe integration and updating the favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey =
+        "pk_test_51Hus9hBzjP6PSJ5FpkRbiY5M63vyiovdwJdPIMvCW93nRkfCQOGmOXOWo33ywcjgzZ48YhbNkLGJcvOzMK1XdvI600oqQQNTXf";
+
+    const onToken = (token) => {
+        console.log(token);
+        alert("Payment Successful");
+    };
+
+    return (
+        <StripeCheckout
+            label="Pay Now"
+            name="3DG3 Clothing Ltd."
+            billingAddress
+            shippingAddress
+            image="https://sendeyo.com/up/d/f3eb2117da"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
