be9aee1336a5d2b1b2860ce8342644571e17f26a
Tracy Marshall <tracythedev@gmail.com>
2020-12-10 05:39:58 -0500

    quick backup push

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 ### A Create React App Project
 
 #### To-Do:
-* Check store.js at the export default. I may need to set the object to a variable first
+* Check store.js at the export default. I may need to set the object to a 
+    variable first
+* Change the rest of the CSS in the app to styled-components
+* Fix the styling on the Google sign-in button
+* Make the titles for the collections in collection preview links, to go to the 
+    single collection page
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,43 +3,77 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "AIzaSyCnn7YHmcgoFUrIn-uNl8ZZvJQ-NImtLog",
-  authDomain: "dg3-db.firebaseapp.com",
-  databaseURL: "https://dg3-db.firebaseio.com",
-  projectId: "dg3-db",
-  storageBucket: "dg3-db.appspot.com",
-  messagingSenderId: "414214371477",
-  appId: "1:414214371477:web:2fd54e9f1dcc5dbb4f039c",
-  measurementId: "G-N563L7S1NR",
-};
-
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
-
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
-
-  const snapShot = await userRef.get();
-
-  if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
-
-    try {
-      await userRef.set({
-        displayName,
-        email,
-        createdAt,
-        ...additionalData,
-      });
-    } catch (error) {
-      console.log("error creating user", error.message);
-    }
-  }
-
-  return userRef;
+    apiKey: "AIzaSyCnn7YHmcgoFUrIn-uNl8ZZvJQ-NImtLog",
+    authDomain: "dg3-db.firebaseapp.com",
+    databaseURL: "https://dg3-db.firebaseio.com",
+    projectId: "dg3-db",
+    storageBucket: "dg3-db.appspot.com",
+    messagingSenderId: "414214371477",
+    appId: "1:414214371477:web:2fd54e9f1dcc5dbb4f039c",
+    measurementId: "G-N563L7S1NR",
 };
 
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log("error creating user", error.message);
+        }
+    }
+
+    return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+    collectionKey,
+    objectsToAdd
+) => {
+    const collectionRef = firestore.collection(collectionKey);
+
+    const batch = firestore.batch();
+
+    objectsToAdd.forEach((obj) => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
+
+    return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map((doc) => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        }
+    });
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    }, {})
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
