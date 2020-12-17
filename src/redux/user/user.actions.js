54e833d9c0bab35506208719923b6c4fe45e0bc9
Tracy Marshall <tracythedev@gmail.com>
2020-12-17 17:45:55 -0500

    created user and cart sagas for sign in, sign up and sign in with goole

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,35 +1,51 @@
 import UserActionTypes from "./user.types";
 
-export const setCurrentUser = (user) => ({
-  type: UserActionTypes.SET_CURRENT_USER,
-  payload: user,
+export const googleSignInStart = () => ({
+    type: UserActionTypes.GOOGLE_SIGN_IN_START,
 });
 
-export const googleSignInStart = () => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_START
-})
+export const signInSuccess = (user) => ({
+    type: UserActionTypes.SIGN_IN_SUCCESS,
+    payload: user,
+});
 
-export const googleSignInSuccess = user => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
-  payload: user
-})
+export const signInFailure = (error) => ({
+    type: UserActionTypes.SIGN_IN_FAILURE,
+    payload: error,
+});
 
-export const googleSignInFailure = error => ({
-  type: UserActionTypes.GOOGLE_SIGN_IN_FAILURE,
-  payload: error
-})
+export const emailSignInStart = (emailAndPassword) => ({
+    type: UserActionTypes.EMAIL_SIGN_IN_START,
+    payload: emailAndPassword,
+});
 
-export const emailSignInStart = emailAndPassword => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_START,
-  payload: emailAndPassword
-})
+export const checkUserSession = () => ({
+    type: UserActionTypes.CHECK_USER_SESSION,
+});
 
-export const emailSignInSuccess = user => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_SUCCESS,
-  payload: user
-})
+export const signOutStart = () => ({
+    type: UserActionTypes.SIGN_OUT_START,
+});
 
-export const emailSignInFailure = error => ({
-  type: UserActionTypes.EMAIL_SIGN_IN_FAILURE,
-  payload: error
-})
+export const signOutSuccess = () => ({
+    type: UserActionTypes.SIGN_OUT_SUCCESS,
+});
+
+export const signOutFailure = (error) => ({
+    type: UserActionTypes.SIGN_OUT_FAILURE,
+    payload: error,
+});
+
+export const signUpStart = (userDetails) => ({
+    type: UserActionTypes.SIGN_UP_START,
+    payload: userDetails
+});
+
+// export const signUpSuccess = () => ({
+//     type: UserActionTypes.SIGN_UP_SUCCESS,
+// });
+
+export const signUpFailure = (error) => ({
+    type: UserActionTypes.SIGN_UP_FAILURE,
+    payload: error,
+});
