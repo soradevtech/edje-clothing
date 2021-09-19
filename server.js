46d2cf5ac594f2f6d6ad6205729636991949c917
Tracy Marshall <tracythedev@gmail.com>
2021-09-19 04:02:45 -0400

    fixed service worker setup and server.js, and added icons

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const compression = require("compression");
+const enforce = require('express-sslify')
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
