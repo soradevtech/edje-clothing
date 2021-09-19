80648ddeabf7952b1065209669e231cacc8e9474
Tracy Marshall <tracythedev@gmail.com>
2021-09-19 03:38:28 -0400

    updating our code to be pwa compliant, added service worker, https, updated manifest

--- server.js
+++ server.js
@@ -2,46 +2,51 @@ const express = require("express");
 // const cors = require("cors"); // will remove if the app works without it
 const bodyParser = require("body-parser");
 const path = require("path");
-const compression = require('compression');
+const compression = require("compression");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = new express();
 const port = process.env.PORT || 5000;
 
-app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // app.use(cors()); // will remove if the app works without it
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
+if (process.env.NODE_ENV === "production") {
+  app.use(compression);
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
-app.listen(port, error => {
-    if(error) throw error;
-    console.log('Server running on port' + port)
-})
+app.listen(port, (error) => {
+  if (error) throw error;
+  console.log("Server running on port" + port);
+});
 
-app.post('/payment', (req, res) => {
-    const body = {
-        source: req.body.token.id,
-        amount: req.body.amount,
-        currency: 'usd'
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "serviceworker.js"));
+});
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd",
+  };
+
+  stripe.charges.create(body, (stripeError, stripeRes) => {
+    if (stripeError) {
+      res.status(500).send({ error: stripeError });
+    } else {
+      res.status(200).send({ success: stripeRes });
     }
-
-    stripe.charges.create(body, (stripeError, stripeRes) => {
-        if(stripeError) {
-            res.status(500).send({error: stripeError});
-        } else {
-            res.status(200).send({success: stripeRes})
-        }
-    })
-})
+  });
+});
